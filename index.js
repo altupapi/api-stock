4fa58260ab2296812521cbaadd76011bd5165389
Lenny_Seele_X <lenny.mel2612@gmail.com>
2026-07-06 11:52:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/api/compras-proveedor', (req, res) => {
 
     detalles.forEach(item => {
         // 🌟 CLAVE: Aquí SUMAMOS al stock actual porque es una ENTRADA
-        const querySumarStock = 'UPDATE productos SET cantidad = cantidad + ? WHERE codigo = ?';
+        const querySumarStock = 'UPDATE productos SET cantidad = cantidad + ? WHERE sku = ?';
         
         db.query(querySumarStock, [parseInt(item.cantidad), item.producto_id], (err, result) => {
             consultasCompletadas++;
